f55c177a55fe315d07133256d9b21975b0321571
Jerry Jäppinen <eiskis@gmail.com>
2022-07-30 22:21:45 +0200

    Updated docs

--- pages/linna-vue/nuxt.config/viewport.vue
+++ pages/linna-vue/nuxt.config/viewport.vue
@@ -1,5 +1,17 @@
+<script setup>
+const sampleOptions = {
+  width: 'device-width',
+  initialScale: 1,
+  themeColor: '#123abc'
+}
+</script>
+
 <template>
-  <NuxtConfigPage name="viewport">
+  <NuxtConfigPage
+    name="viewport"
+    args="options"
+    :args-sample="sampleOptions"
+  >
     <p>Add common viewport meta tags and mobile zooming options.</p>
   </NuxtConfigPage>
 </template>
